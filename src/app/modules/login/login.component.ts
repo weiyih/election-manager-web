101bdfe1f56ff97f52d1700890734fc9ecd5b886
Kevin <weiyih@sheridancollege.ca>
2020-10-21 17:13:01 -0400

    wip login page

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs/operators';
 import { AuthenticationService } from '../../core/auth/authentication.service';
 
 @Component({
@@ -11,7 +12,7 @@ import { AuthenticationService } from '../../core/auth/authentication.service';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   loginUrl: string;
-  error = '';
+  loginError = '';
   hidePassword = true;
 
   constructor(
@@ -25,12 +26,22 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
+      username: ['', Validators.required, Validators.email],
       password: ['', Validators.required],
     });
 
     // this.loginUrl =
   }
+
+  //   getErrorMessage() {
+  //     this.loginForm.controls
+  //     if (this.loginForm.username.hasError('required')) {
+  //       return 'You must enter a value';
+  //     }
+
+  //     return this.email.hasError('email') ? 'Not a valid email' : '';
+  //   }
+  // }
 
   // Login button submit
   onSubmit(): void {
@@ -42,7 +53,11 @@ export class LoginComponent implements OnInit {
     if (this.loginForm.invalid) {
       return;
     }
-    // this.authService.login(username, password)
+    this.authService.login(username, password)
+      .pipe(first())
+      .subscribe((data) => {
+        this.router.navigate(['main']);
+      });
     // .subscribe(
     //   () => {
     // Navigate to the main page

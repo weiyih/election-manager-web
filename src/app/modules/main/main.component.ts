340af66b8b9a4986869643e0a2db06aa07ef3c78
Kevin <weiyih@sheridancollege.ca>
2020-10-21 17:11:34 -0400

    updated main css to use prebuilt themes, removed css files

--- src/app/modules/main/main.component.ts
+++ src/app/modules/main/main.component.ts
@@ -4,7 +4,6 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
 })
 
 export class MainComponent implements OnInit {

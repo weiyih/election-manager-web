b58486b6ef9f20132e958586b9367c2e5dd49812
Kevin <weiyih@sheridancollege.ca>
2020-11-28 03:56:57 -0500

    login components

--- src/app/core/auth/authentication.service.ts
+++ src/app/core/auth/authentication.service.ts
@@ -5,29 +5,62 @@ import {
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { BehaviorSubject, Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+
+// HTTP client service uses observables
+import { BehaviorSubject, Observable, throwError,  } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import { Account } from '@app/model/account';
+import { environment } from '@environments';
 
 @Injectable({ providedIn: 'root' })
-export class AuthenticationService {
-  constructor(
-    private http: HttpClient,
-    private router: Router) {}
+export class AuthService {
+  private authSubject: BehaviorSubject<boolean>;
+  public isAuthenticated: Observable<boolean>;
 
-
-  public isAuthenticated = new BehaviorSubject<boolean>(false);
-
-  login(username: string, password: string): void {
-    // const login_url = 'http://kevinwei.ca/admin/login';
-    // return this.http.post(login_url)
-    // Retrieve JWT Token
-    // Store JWT token in local storage
+  constructor(private http: HttpClient, private router: Router) {
+    this.authSubject = new BehaviorSubject<boolean>(this.hasToken());
+    this.isAuthenticated = this.authSubject.asObservable();
   }
 
-  // Logout
-  // Removes user from local storage and redirects to login
+  // Login with username and password
+  login(username: string, password: string) {
+    const loginUrl = `${environment.apiUrl}/login`;
+
+    const body = { username: username, password: password };
+
+    return this.http.post<Account>(loginUrl, body)
+      .pipe(
+        map( account => {
+          localStorage.setItem('token': 'JWT');
+          this.authSubject.next(true);
+          return account;
+          }),
+        catchError(this.handleError)
+      );
+
+  }
+
+  // Logout - removes user from local storage and redirects to login
   logout(): void {
-    localStorage.clear();     // TODO - Remove only user data from local storage?
+    localStorage.clear();
+    this.authSubject.next(false);
     this.router.navigate(['']);
+  }
+
+  // Check if JWT token is stored in local storage
+  private hasToken(): boolean {
+    return !!localStorage.getItem('token');
+  }
+
+  private handleError(error: HttpErrorResponse): Observable<Error> {
+
+    const userMessage = `Login failed.`;
+    if (error.error instanceof ErrorEvent) {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    return throwError(`${userMessage}: ${error.error}`);
   }
 }

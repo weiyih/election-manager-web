4fb22c7c5d4c6e83265ea2ad2a2d5ef929e49e60
Kevin <weiyih@sheridancollege.ca>
2020-11-30 00:22:27 -0500

    edit component wip

--- web/src/app/modules/election/edit/edit.component.ts
+++ web/src/app/modules/election/edit/edit.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Election } from '@app/model/election';
 import { Candidate } from '@app/model/candidate';
@@ -7,6 +12,7 @@ import { AuthService } from '@core/auth/authentication.service';
 import { ElectionService } from '@services/election.services';
 import { Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-edit',
@@ -19,57 +25,59 @@ export class EditComponent implements OnInit {
   loading = false;
   submitted = false;
 
-  election: Observable<Election>;
+  // Election Object
+  election: Election[] = [];
   electionId: string;
+  minEndDate: Date;
 
   constructor(
     private electionServices: ElectionService,
-
+    private route: ActivatedRoute,
     private formBuilder: FormBuilder,
-    private router: ActivatedRoute,
+    private router: ActivatedRoute
   ) {
+    this.router.queryParams.subscribe((params) => {
+      this.electionId = params.id;
+    });
+
+    // Currently send the minEndDate to be at least 1 day
+    const currentDate = moment();
+    this.minEndDate = currentDate.add(1, 'day').toDate();
     // TODO - Navigate if authenticated
   }
 
   ngOnInit(): void {
     // Retrieve election id from ParamMap
-    this.router.queryParams.subscribe(params => {
-      this.electionId = params.id;
-    });
 
     // this.loadElection(this.electionId);
 
     // Election Form
     // TODO - Populate based on the observed election object
     this.electionForm = this.formBuilder.group({
-        // election_id: [null, Validators.required],
-        election_name: [null, Validators.required],
-        election_start_date: [null, Validators.required],
-        election_end_date: [null, Validators.required],
-        advanced_polling: [null, Validators.required],
-        advanced_start_date: [null, Validators.required],
-        advanced_end_date: [null, Validators.required],
-        channel_name: [null, Validators.required],
-        contract_name: [null, Validators.required],
-        // created_at: [null, Validators.required],
-        // updated_at: [null, Validators.required],
-        // locked: [null, Validators.required], // 0 - unlocked(editable), 1 - locked(not running/in progress/completed)
-        // progress: [null, Validators.required], // 0 - not running, 1 - in progress, 2 - completed
-        // disabled: [null, Validators.required], // 0 - deleted election, 1 - valid election
+      // election_id: [null, Validators.required],
+      election_name: [null, Validators.required],
+      election_start_date: [null, Validators.required],
+      election_end_date: [null, Validators.required],
+      advanced_polling: [null, Validators.required],
+      advanced_start_date: [null, Validators.required],
+      advanced_end_date: [null, Validators.required],
+      channel_name: [null, Validators.required],
+      contract_name: [null, Validators.required],
+      // created_at: [null, Validators.required],
+      // updated_at: [null, Validators.required],
+      // locked: [null, Validators.required], // 0 - unlocked(editable), 1 - locked(not running/in progress/completed)
+      // progress: [null, Validators.required], // 0 - not running, 1 - in progress, 2 - completed
+      // disabled: [null, Validators.required], // 0 - deleted election, 1 - valid election
     });
 
     // electionName
-
-
   }
-
 
   // Retrieves elections from DB and sets the datasource
   // Ensures the most up to date information
   private loadElection(electionId): void {
     console.log('LOADING...');
     this.electionServices.getElection(electionId).subscribe((election) => {
-      console.log(election);
       this.election = election;
     });
   }
@@ -77,7 +85,7 @@ export class EditComponent implements OnInit {
   // Convenience getter to access form fields
   // tslint:disable-next-line: typedef
   get form() {
-    return this.loginForm.controls;
+    return this.electionForm.controls;
   }
 
   // Login
@@ -89,7 +97,5 @@ export class EditComponent implements OnInit {
     }
 
     // validate candidates
-
-
   }
 }
